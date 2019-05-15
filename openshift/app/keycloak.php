72e192163a11ef09559d188461fdb487ec3b94ab
Antonio J. del Aguila <ajdelaguila@crossvale.com>
2019-05-15 02:25:17 -0400

    parameterize resource type for the php test

--- openshift/app/keycloak.php
+++ openshift/app/keycloak.php
@@ -4,7 +4,7 @@ header('Content-Type: application/json');
 $config = array(
     'realm' => 'nisarealm',
     'auth-server-url' => $_ENV["KEYCLOAK_URL"],
-    'resource' => "app2"
+    'resource' => $_ENV["OAUTH_RESOURCE"]
 );
 
 echo json_encode($config, JSON_UNESCAPED_SLASHES);
